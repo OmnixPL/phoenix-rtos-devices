2011d2aec8e2d5f6bd625279e2f07757882dbfbf
Lukasz Kosinski <lukasz.kosinski@phoenix-rtos.com>
2020-03-31 22:05:34 +0200

    pc-ide: added ATA/ATAPI devices detection

--- storage/pc-ide/idedrv.c
+++ storage/pc-ide/idedrv.c
@@ -13,6 +13,7 @@
 
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 #include <sys/list.h>
 
 #include <arch/ia32/io.h>
@@ -21,6 +22,7 @@
 
 
 struct {
+	unsigned int devices;
 	bus_t *buses;
 } ide_common;
 
@@ -82,6 +84,189 @@ static void detect_reg(reg_t *reg, pci_resource_t *res)
 }
 
 
+static uint8_t read_status(channel_t *channel)
+{
+	return channel->base.readb(channel->base.addr, REG_STATUS);
+}
+
+
+static uint8_t read_altstatus(channel_t *channel)
+{
+	return channel->ctrl.readb(channel->ctrl.addr, REG_CTRL);
+}
+
+
+static void write_cmd(channel_t *channel, uint8_t cmd)
+{
+	channel->base.writeb(channel->base.addr, REG_STATUS, cmd);
+}
+
+
+static void delay_400ns(channel_t *channel)
+{
+	read_altstatus(channel);
+	read_altstatus(channel);
+	read_altstatus(channel);
+	read_altstatus(channel);
+}
+
+
+static void soft_reset(channel_t *channel)
+{
+	/* Software reset all drives on the bus */
+	channel->ctrl.writeb(channel->ctrl.addr, REG_CTRL, CTRL_SOFTRST | CTRL_NIEN);
+	/* Short delay */
+	usleep(10);
+	/* Reset the bus to normal operation */
+	channel->ctrl.writeb(channel->ctrl.addr, REG_CTRL, CTRL_NIEN);
+	/* Short delay */
+	usleep(10);
+}
+
+
+static void enable_irq(channel_t *channel)
+{
+	channel->ctrl.writeb(channel->ctrl.addr, REG_CTRL, 0);
+}
+
+
+static void disable_irq(channel_t *channel)
+{
+	channel->ctrl.writeb(channel->ctrl.addr, REG_CTRL, CTRL_NIEN);
+}
+
+
+static uint16_t read_data(channel_t *channel)
+{
+	uint16_t data = channel->base.readw(channel->base.addr, REG_DATA);
+	return ((data & 0xFF) << 8) | ((data >> 8) & 0xFF);
+}
+
+
+static uint8_t read_error(channel_t *channel)
+{
+	return channel->base.readb(channel->base.addr, REG_ERROR);
+}
+
+
+static uint8_t read_secnum(channel_t *channel)
+{
+	return channel->base.readw(channel->base.addr, REG_SECNUM);
+}
+
+
+static uint8_t read_sec(channel_t *channel)
+{
+	return channel->base.readw(channel->base.addr, REG_SEC);
+}
+
+
+static uint8_t read_lcyl(channel_t *channel)
+{
+	return channel->base.readw(channel->base.addr, REG_LCYL);
+}
+
+
+static uint8_t read_hcyl(channel_t *channel)
+{
+	return channel->base.readw(channel->base.addr, REG_HCYL);
+}
+
+
+static uint8_t read_head(channel_t *channel)
+{
+	return channel->base.readw(channel->base.addr, REG_HEAD) & 0x0F;
+}
+
+
+static void select_device(channel_t *channel, uint8_t devnum, uint16_t secnum, uint64_t lba, uint8_t mode)
+{
+	dev_t *device = channel->devices + devnum;
+	uint8_t devsel = DEVSEL_SET0 | DEVSEL_SET1;
+	uint32_t c = 0, h = 0, s = 0;
+
+	/* Select device: master or slave */
+	if (devnum)
+		devsel |= DEVSEL_DEVNUM;
+
+	/* Select addressing mode */
+	switch (mode) {
+	case AMODE_CHS:
+		if (lba != -1) {
+			/* Convert LBA to CHS */
+			c = lba / ((uint64_t)(device->heads) * device->sectors);
+			h = (lba / device->sectors) % device->heads;
+			s = (lba % device->sectors) + 1;
+		}
+		devsel |= h & DEVSEL_HEAD;
+
+		channel->base.writeb(channel->base.addr, REG_SECNUM, secnum & 0xFF);
+		channel->base.writeb(channel->base.addr, REG_SEC, s & 0xFF);
+		channel->base.writeb(channel->base.addr, REG_LCYL, c & 0xFF);
+		channel->base.writeb(channel->base.addr, REG_HCYL, (c >> 8) & 0xFF);
+		break;
+
+	case AMODE_LBA28:
+		devsel |= DEVSEL_LBA | ((lba >> 24) & DEVSEL_HEAD);
+
+		channel->base.writeb(channel->base.addr, REG_SECNUM, secnum & 0xFF);
+		channel->base.writeb(channel->base.addr, REG_SEC, lba & 0xFF);
+		channel->base.writeb(channel->base.addr, REG_LCYL, (lba >> 8) & 0xFF);
+		channel->base.writeb(channel->base.addr, REG_HCYL, (lba >> 16) & 0xFF);
+		break;
+
+	case AMODE_LBA48:
+		devsel |= DEVSEL_LBA;
+
+		channel->base.writeb(channel->base.addr, REG_SECNUM, (secnum >> 8) & 0xFF);
+		channel->base.writeb(channel->base.addr, REG_SECNUM, secnum & 0xFF);
+		channel->base.writeb(channel->base.addr, REG_SEC, (lba >> 24) & 0xFF);
+		channel->base.writeb(channel->base.addr, REG_SEC, lba & 0xFF);
+		channel->base.writeb(channel->base.addr, REG_LCYL, (lba >> 32) & 0xFF);
+		channel->base.writeb(channel->base.addr, REG_LCYL, (lba >> 8) & 0xFF);
+		channel->base.writeb(channel->base.addr, REG_HCYL, (lba >> 40) & 0xFF);
+		channel->base.writeb(channel->base.addr, REG_HCYL, (lba >> 16) & 0xFF);
+		break;
+	}
+
+	/* Select the device */
+	channel->base.writeb(channel->base.addr, REG_DEVSEL, devsel);
+	/* Wait for the device to push its status onto the bus */
+	delay_400ns(channel);
+	/* Write features */
+	channel->base.writeb(channel->base.addr, REG_FEATURES, 0);
+}
+
+
+static void wait_bsy(channel_t *channel)
+{
+	while (read_status(channel) & STATUS_BSY);
+}
+
+
+static int wait_drq(channel_t *channel)
+{
+	uint8_t status;
+
+	while (!((status = read_status(channel)) & STATUS_DRQ)) {
+		if (status & STATUS_ERR)
+			return -1;
+	}
+
+	return 0;
+}
+
+
+static void wait_rdy(channel_t *channel)
+{
+	uint8_t status;
+
+	do {
+		status = read_status(channel);
+	} while ((status & STATUS_DRQ) || !(status & STATUS_RDY));
+}
+
+
 int ide_init(uint8_t mode)
 {
 	static pci_id_t pci_ide[] = {
@@ -90,11 +275,17 @@ int ide_init(uint8_t mode)
 
 	platformctl_t pctl = { 0 };
 	bus_t *bus;
-	int i, devices = 0;
+	channel_t *channel;
+	dev_t *device;
+	uint8_t lcyl, hcyl, *info;
+	int i, j, k, l;
 
-	memset(&idedrv_common, 0, sizeof(idedrv_common));
+	memset(&ide_common, 0, sizeof(idedrv_common));
 	pctl.action = pctl_get;
 	pctl.type = pctl_pci;
+
+	if (!(info = malloc(512)))
+		return -ENOMEM;
 
 	/* Find IDE bus devices */
 	for (i = 0; i < sizeof(pci_ide) / sizeof(pci_id_t); i++) {
@@ -136,6 +327,112 @@ int ide_init(uint8_t mode)
 				bus->channels[1].irq = SECONDARY_IRQ;
 			}
 			detect_reg(&(bus->channels[1].dma), bus->device.resources + 4);
+		
+			/* Loop over channels */
+			for (j = 0; j < 2; j++) {
+				channel = bus->channels + j;
+				channel->devices[0].exists = 0;
+				channel->devices[1].exists = 0;
+
+				/* Floating bus check */
+				if (read_status(channel) == 0xFF)
+					continue;
+
+				/* Software reset the bus */
+				soft_reset(channel);
+
+				/* Loop over attached devices */
+				for (k = 0; k < 2; k++) {
+					device = channel->devices + k;
+
+					/* Select the device */
+					select_device(channel, k, 0, -1, AMODE_CHS);
+
+					/* Floating bus check */
+					if (read_status(channel) == 0xFF)
+						continue;
+
+					/* Send identify command */
+					channel->base.writeb(channel->base.addr, REG_CMD, CMD_IDENTIFY);
+
+					if (!read_status(channel))
+						continue;
+
+					/* Wait while busy */
+					wait_bsy(channel);
+
+					if (!(lcyl = read_lcyl(channel)) && !(hcyl = read_hcyl(channel)) && !wait_drq(channel)) {
+						device->type = ATA;
+					}
+					else if ((lcyl == 0x14 && hcyl == 0xEB) || (lcyl == 0x69 && hcyl == 0x96)) {
+						device->type = ATAPI;
+
+						/* Send identify packet command */
+						channel->base.writeb(channel->base.addr, REG_STATUS, CMD_IDENTIFY_PACKET);
+
+						/* Wait while busy */
+						wait_bsy(channel);
+
+						if (wait_drq(channel))
+							continue;
+					}
+					else {
+						/* Neither ATA nor ATAPI */
+						continue;
+					}
+
+					/* Read device identification */
+					for (l = 0; l < 256; l++)
+						((uint16_t *)info)[l] = read_data(channel);
+
+					wait_rdy(channel);
+
+					device->exists = 1;
+					device->dma = info[98] & 1;
+
+					if (device->type == ATA) {
+						device->mode = ((info[98] >> 1) & 1) + ((info[166] >> 2) & 1);
+						device->cylinders = (info[3]  << 0) | (info[2]  << 8);
+						device->heads     = (info[7]  << 0) | (info[6]  << 8);
+						device->sectors   = (info[13] << 0) | (info[12] << 8);
+
+						switch (device->mode) {
+						case AMODE_CHS:
+							device->size =
+								(uint64_t)(device->cylinders) *
+								(uint64_t)(device->heads) *
+								(uint64_t)(device->sectors);
+							break;
+
+						case AMODE_LBA28:
+							device->size =
+								(info[121] <<  0)|
+								(info[120] <<  8)|
+								(info[123] << 16)|
+								(info[122] << 24);
+							break;
+
+						case AMODE_LBA48:
+							device->size =
+								(((uint64_t)info[201]) <<  0)|
+								(((uint64_t)info[200]) <<  8)|
+								(((uint64_t)info[203]) << 16)|
+								(((uint64_t)info[202]) << 24)|
+								(((uint64_t)info[205]) << 32)|
+								(((uint64_t)info[204]) << 40)|
+								(((uint64_t)info[207]) << 48)|
+								(((uint64_t)info[206]) << 56);
+							break;
+						}
+						device->size *= 512;
+					}
+					ide_common.devices++;
+				}
+			}
+			LIST_ADD(&ide_common.buses, bus);
 		}
 	}
+	free(info);
+
+	return EOK;
 }
--- storage/pc-ide/idedrv.h
+++ storage/pc-ide/idedrv.h
@@ -20,30 +20,110 @@
 #include <phoenix/arch/ia32.h>
 
 
-/* IDE operation modes */
+/* Driver base definitions */
+#define PCI_STORAGE_IDE 0x0101 /* IDE PCI class code */
+#define PRIMARY_BASE    0x1F0  /* Primary channel legacy base address */
+#define PRIMARY_CTRL    0x3F6  /* Primary channel legacy ctrl address */
+#define PRIMARY_IRQ     0x0E   /* Primary channel legacy interrupt number */
+#define SECONDARY_BASE  0x170  /* Secondary channel legacy base address */
+#define SECONDARY_CTRL  0x376  /* Secondary channel legacy ctrl address */
+#define SECONDARY_IRQ   0x0F   /* Secondary channel legacy interrupt number */
+
+
+/* Driver operation modes */
 enum { MODE_POLL, MODE_IRQ, MODE_DMA };
 
 
-/* Driver definitions */
-#define PCI_STORAGE_IDE 0x0101
-#define PRIMARY_BASE    0x1F0
-#define PRIMARY_CTRL    0x3F6
-#define PRIMARY_IRQ     0x0E
-#define SECONDARY_BASE  0x170
-#define SECONDARY_CTRL  0x376
-#define SECONDARY_IRQ   0x0F
+/* Supported IDE devices */
+enum { ATA, ATAPI };
+
+
+/* ATA addressing modes */
+enum { AMODE_CHS, AMODE_LBA28, AMODE_LBA48 };
+
+
+/* ATA commands */
+enum {
+	CMD_NOP             = 0x00,
+	CMD_READ_PIO        = 0x20,
+	CMD_READ_PIO_EXT    = 0x24,
+	CMD_READ_DMA_EXT    = 0x25,
+	CMD_WRITE_PIO       = 0x30,
+	CMD_WRITE_PIO_EXT   = 0x34,
+	CMD_WRITE_DMA_EXT   = 0x35,
+	CMD_PACKET          = 0xA0,
+	CMD_IDENTIFY_PACKET = 0xA1,
+	CMD_READ_DMA        = 0xC8,
+	CMD_WRITE_DMA       = 0xCA,
+	CMD_CACHE_FLUSH     = 0xE7,
+	CMD_CACHE_FLUSH_EXT = 0xEA,
+	CMD_IDENTIFY        = 0xEC
+};
+
+
+/* ATA registers */
+enum {
+	REG_CTRL      = 0x0,       /* Control register */
+	REG_ALTSTATUS = 0x0,       /* Alternate status */
+	REG_DATA      = 0x0,       /* R/W PIO data port */
+	REG_ERROR     = 0x1,       /* Read error */
+	REG_FEATURES  = 0x1,       /* Write features */
+	REG_SECNUM    = 0x2,       /* Sectors number */
+	REG_SEC       = 0x3,       /* Sector */
+	REG_LCYL      = 0x4,       /* Cylinder low */
+	REG_HCYL      = 0x5,       /* Cylinder high */
+	REG_HEAD      = 0x6,       /* LBA head */
+	REG_DEVSEL    = 0x6,       /* Drive select */
+	REG_CMD       = 0x7,       /* Write command */
+	REG_STATUS    = 0x7        /* Read status */
+};
+
+
+/* REG_CTRL layout */
+enum {
+	CTRL_NIEN     = 0x02,      /* Not Interrupt ENabled */
+	CTRL_SOFTRST  = 0x04,      /* Software reset all ATA devices on the bus */
+	CTRL_HOB      = 0x80       /* Read back the High Order Byte of the last LBA48 value sent */
+};
+
+
+/* REG_DEVSEL layout */
+enum {
+	DEVSEL_HEAD   = 0x0F,      /* LBA head */
+	DEVSEL_DEVNUM = 0x10,      /* 0: Master, 1: Slave */
+	DEVSEL_SET0   = 0x20,      /* Should always be set */
+	DEVSEL_LBA    = 0x40,      /* 0: CHS, 1: LBA */
+	DEVSEL_SET1   = 0x80       /* Should always be set */
+};
+
+
+/* REG_STATUS layout */
+enum {
+	STATUS_ERR    = 0x01,      /* Error occured for last command */
+	STATUS_DRQ    = 0x08,      /* PIO data is ready */
+	STATUS_SRV    = 0x10,      /* Overlapped mode service request */
+	STATUS_DF     = 0x20,      /* Device fault (doesn't set ERR) */
+	STATUS_RDY    = 0x40,      /* Device is ready */
+	STATUS_BSY    = 0x80       /* Device is busy */
+};
 
 
 typedef struct channel_t channel_t;
 
 
 typedef struct {
-	uint8_t exists;        /* Device detected */
-	uint8_t type;          /* 0: ATA, 1: ATAPI */
-	uint8_t mode;          /* 0: CHS, 1: LBA28, 2: LBA48 */
-	uint8_t dma;           /* DMA support */
-	uint64_t size;         /* Storage size */
-	channel_t *channel;    /* IDE channel */
+	uint8_t exists;            /* Device detected */
+	uint8_t type;              /* 0: ATA, 1: ATAPI */
+	uint8_t mode;              /* 0: CHS, 1: LBA28, 2: LBA48 */
+	uint8_t dma;               /* DMA support */
+
+	/* Device geometry */
+	uint32_t cylinders;        /* Cylinders number */
+	uint32_t heads;            /* Heads number */
+	uint32_t sectors;          /* Sectors number */
+	uint64_t size;             /* Storage size */
+
+	channel_t *channel;        /* IDE channel */
 } dev_t;
 
 
@@ -60,29 +140,29 @@ typedef struct bus_t bus_t;
 
 
 struct channel_t {
-	bus_t *bus;            /* IDE bus */
-	dev_t devices[2];      /* IDE devices */
+	bus_t *bus;                /* IDE bus */
+	dev_t devices[2];          /* IDE devices */
 
 	/* Channel IO-ports / memory mappings */
-	reg_t base;            /* Command registers access */
-	reg_t ctrl;            /* Control resgister access */
-	reg_t dma;             /* PCI DMA registers access */
+	reg_t base;                /* Command registers access */
+	reg_t ctrl;                /* Control resgister access */
+	reg_t dma;                 /* PCI DMA registers access */
 
 	/* Interrupts handling */
-	unsigned char irq;     /* Interrupt number */
-	handle_t cond;         /* Interrupt condition variable */
-	handle_t inth;         /* Interrupt handle */
+	unsigned char irq;         /* Interrupt number */
+	handle_t cond;             /* Interrupt condition variable */
+	handle_t inth;             /* Interrupt handle */
 
 	/* Synchronization */
-	handle_t lock;         /* Channel access mutex */
+	handle_t lock;             /* Channel access mutex */
 };
 
 
 struct bus_t {
-	pci_device_t device;   /* IDE bus device */
-	channel_t channels[2]; /* IDE bus channels */
+	pci_device_t device;       /* IDE bus device */
+	channel_t channels[2];     /* IDE bus channels */
 
-	void *prev, *next;     /* Doubly linked list */
+	void *prev, *next;         /* Doubly linked list */
 };
 
 
